cef7fa1d45ba5513ad220aa64b3b8eb98728acc4
An Nguyen <An_Nguyen@epam.com>
2020-06-18 16:35:03 +0700

    add buttons

--- src/components/PopularList/index.js
+++ src/components/PopularList/index.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { ButtonGroup } from 'reactstrap'
 import { PopularContext } from '../../store/PopularList';
 import { getPopularMovieList } from '../../services/PopularList.services'
 import PopularItem from './item';
 import './styles.scss';
 
 function Popular() {
-  
+
   const { state, dispatch } = React.useContext(PopularContext);
   const [popularList, setPopularList] = useState([]);
-
+  const [isActiveTV, setIsActiveTV] = useState(true);
   useEffect(() => {
     getPopularMovieList()
       .then(response => response.json())
@@ -18,10 +19,23 @@ function Popular() {
   useEffect(() => {
     setPopularList(state?.popularList?.results);
   }, [state, state.popularList]);
-
+ const onToggle = (type) => {
+   if(type === 'tv'){
+     setIsActiveTV(true)
+   }else{
+    setIsActiveTV(false)
+   }
+ }
   return (
     <div className="popular-container">
-      <h2>What's Populars</h2>
+      <div className="popular-container__control">
+        <h2>What's Populars</h2>
+        <ButtonGroup aria-label="Basic example"> 
+          <div className={isActiveTV ? "select-active" : "select-disbled"} onClick={() => onToggle('tv')}>On TV</div>
+          <div className={!isActiveTV ? "select-active" : "select-disbled"} onClick={() => onToggle('theaters')}>In Theaters</div>
+        </ButtonGroup>
+      </div>
+
       <div className="popular-list">
         {
           popularList && popularList.length > 0 && popularList.map((item, index) =>
