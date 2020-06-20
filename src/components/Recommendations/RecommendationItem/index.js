7d151abe0530b74773adc1c75f2c338e89bb9041
An Nguyen <An_Nguyen@epam.com>
2020-06-20 16:37:11 +0700

    fix bugs

--- src/components/Recommendations/RecommendationItem/index.js
+++ src/components/Recommendations/RecommendationItem/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Link, useHistory  } from "react-router-dom";
+import { replaceAll } from "../../../utils/customString";
 import "./styles.scss";
 
 const ORG_URL = "https://image.tmdb.org/t/p/w220_and_h330_face";
@@ -10,36 +12,59 @@ function RecommendationItem({ RecommendItem }) {
   const isMovie = !!(hrefPaths[1] === "movie");
 
   if (isMovie) {
-    const { poster_path, title, release_date, overview } = RecommendItem;
+    const { poster_path, title, release_date, overview, id } = RecommendItem;
     return (
       <div className="recommend-item">
-        <div className="recommend-item__main">
-          <div className="recommend-item__image">
-            <img src={ORG_URL + poster_path} alt="poster" />
+        <Link to={`movie/${id}-${replaceAll(title, " ", "-")}`}>
+          <div className="recommend-item__main">
+            <div className="recommend-item__image">
+              <img src={ORG_URL + poster_path} alt="poster" />
+            </div>
           </div>
-        </div>
-        <div className="recommend-item__content">
-          <div className="recommend-item__content__title">{title}</div>
-          <div className="recommend-item__content__release">{release_date}</div>
-          <div className="recommend-item__over">
-            <span>{overview}</span>
+          <div className="recommend-item__content">
+            <div className="recommend-item__content__title">{title}</div>
+            <div className="recommend-item__content__release">
+              <i className="fa fa-calendar" aria-hidden="true"></i>
+              <span className="recommend-item__content__release__date">
+                {release_date}
+              </span>
+              <i class="fa fa-heartbeat" aria-hidden="true"></i>
+              <i class="fa fa-bookmark" aria-hidden="true"></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
+            </div>
+            <div className="recommend-item__over">
+              <span>{overview}</span>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   } else {
-    const { poster_path, name, first_air_date } = RecommendItem;
+    const { poster_path, name, first_air_date, overview, id } = RecommendItem;
     return (
       <div className="recommend-item">
-        <div className="recommend-item__image">
-          <img src={ORG_URL + poster_path} alt="poster" />
-        </div>
-        <div className="recommend-item__content">
-          <div className="recommend-item__content__title">{name}</div>
-          <div className="recommend-item__content__release">
-            {first_air_date}
+        <Link to={`movie/${id}-${replaceAll(name, " ", "-")}`}>
+          <div className="recommend-item__main">
+            <div className="recommend-item__image">
+              <img src={ORG_URL + poster_path} alt="poster" />
+            </div>
           </div>
-        </div>
+          <div className="recommend-item__content">
+            <div className="recommend-item__content__title">{name}</div>
+            <div className="recommend-item__content__release">
+              <i class="fa fa-calendar" aria-hidden="true"></i>
+              <span className="recommend-item__content__release__date">
+                {first_air_date}
+              </span>
+              <i class="fa fa-heartbeat" aria-hidden="true"></i>
+              <i class="fa fa-bookmark" aria-hidden="true"></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
+            </div>
+            <div className="recommend-item__over">
+              <span>{overview}</span>
+            </div>
+          </div>
+        </Link>
       </div>
     );
   }
